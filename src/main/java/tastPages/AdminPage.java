4234e27dd2e4ec26c8c387d43f453dac40c7dc6a
Olyamyr <troshina1994@bk.ru>
2025-03-24 17:22:11 +0400

    ДЗ 2

--- src/main/java/tastPages/AdminPage.java
+++ src/main/java/tastPages/AdminPage.java
@@ -1,5 +1,41 @@
 package tastPages;
 
-public class AdminPage extends BasePage{
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class AdminPage {
+    //Переход на вкладку Admin
+    public SelenideElement adminTab = $(By.xpath("//span[text()='Admin']"));
+
+    //Нажимаем на кнопку "Add" для добавления нового пользователя
+    public SelenideElement addUserButton = $(By.xpath("//i[@class='oxd-icon bi-plus oxd-button-icon']"));
+
+    //Заполняем поля формы
+    //Роль пользователя
+   public SelenideElement roleField = $(By.xpath("//i[@class='oxd-icon bi-caret-down-fill " +
+            "oxd-select-text--arrow']"));
+
+    //Статус пользователя
+    public SelenideElement statusField = $(By.xpath("//i[@class='oxd-icon bi-caret-down-fill " +
+            "oxd-select-text--arrow']"));
+
+    //Имя сотрудника
+    public SelenideElement firstNameField = $(By.xpath("//div[@class='oxd-autocomplete-text-input " +
+            "oxd-autocomplete-text-input--active']"));
+
+    //Имя пользователя
+    public SelenideElement usernameField = $(By.xpath("//input[@class='oxd-input " +
+            "oxd-input--active oxd-input--error']"));
+
+    //Пароль пользователя
+    public SelenideElement passwordField = $(By.xpath("//input[@type='password']"));
+
+    //Подтверждение пароля
+    public SelenideElement confirmPasswordField = $(By.xpath("//input[@type='confirmpassword']"));
+
+    //Клик по кнопке "Save"
+    public SelenideElement saveButton = $(By.xpath("//button[@type='submit']"));
 
 }
--- src/test/java/lesson21/AdminTest.java
+++ src/test/java/lesson21/AdminTest.java
@@ -3,54 +3,46 @@ package lesson21;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import tastPages.AdminPage;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class AdminTest extends BaseTest {
+
+    AdminPage adminPage = new AdminPage();
+
     //Переход на вкладку Admin
     @Test(priority = 1)
     public void searchAdmin() {
-        $(By.xpath("//span[text()='Admin']")).click();
+        adminPage.adminTab.click();
     }
 
     //Создание пользователя
     @Test(priority = 2)
     public void creatingAUser() {
         //Нажимаем на кнопку "Add" для добавления нового пользователя
-        SelenideElement addUserButton = $(By.xpath("//i[@class='oxd-icon bi-plus oxd-button-icon']"));
-        addUserButton.click();
+        adminPage.addUserButton.click();
 
         //Заполняем поля формы
         //Роль пользователя
-        SelenideElement roleField = $(By.xpath("//i[@class='oxd-icon bi-caret-down-fill " +
-                "oxd-select-text--arrow']"));
-        roleField.selectOptionByValue("ESS");
+        adminPage.roleField.selectOptionByValue("ESS");
 
         //Статус пользователя
-        SelenideElement statusField = $(By.xpath("//i[@class='oxd-icon bi-caret-down-fill " +
-                "oxd-select-text--arrow']"));
-        statusField.selectOptionByValue("Enabled");
+        adminPage.statusField.selectOptionByValue("Enabled");
 
         //Имя сотрудника
-        SelenideElement firstNameField = $(By.xpath("//div[@class='oxd-autocomplete-text-input " +
-                "oxd-autocomplete-text-input--active']"));
-        firstNameField.setValue("Test");
+        adminPage.firstNameField.setValue("Test");
 
         //Имя пользователя
-        SelenideElement usernameField = $(By.xpath("//input[@class='oxd-input " +
-                "oxd-input--active oxd-input--error']"));
-        usernameField.setValue("TestUser");
+        adminPage.usernameField.setValue("TestUser");
 
         //Пароль пользователя
-        SelenideElement passwordField = $(By.xpath("//input[@type='password']"));
-        passwordField.setValue("12345");
+        adminPage.passwordField.setValue("12345");
 
         //Подтверждение пароля
-        SelenideElement confirmPasswordField = $(By.xpath("//input[@type='confirmpassword']"));
-        confirmPasswordField.setValue("12345");
+        adminPage.confirmPasswordField.setValue("12345");
 
         //Клик по кнопке "Save"
-        SelenideElement saveButton = $(By.xpath("//button[@type='submit']"));
-        saveButton.click();
+        adminPage.saveButton.click();
     }
 }
